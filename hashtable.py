304d531f15a2717c0e971c29b3e5d34153e4f181
Hendryboyz <Hendry.js1247@gmail.com>
2020-11-01 21:53:10 +0800

    Handle hash key collision and provide get keys() method

--- hashtable.py
+++ hashtable.py
@@ -3,14 +3,25 @@ class HashTable:
     self.data = [ None ] * size
   
   def get(self, key):
-    index = self.__hash_function(key)
-    return self.data[index][1]
+    address = self.__hash_function(key)
+    current_bucket = self.data[address]
+    if current_bucket:
+      for element in current_bucket:
+        if element[0] == key:
+          return element[1]
+    return None
 
   def set(self, key, val):
     key_value_pair = [key, val]
-    # handle key isExisting
-    index = self.__hash_function(key)
-    self.data[index] = key_value_pair
+    address = self.__hash_function(key)
+    if self.data[address] is None:
+      self.data[address] = [key_value_pair]
+    else:
+      self.data[address].append(key_value_pair)
+    
+  def keys(self):
+    all_buckets = [ bucket for bucket in self.data if bucket ]
+    return [element[0] for each_bucket in all_buckets for element in each_bucket]
 
   def __hash_function(self, key):
     hash = 0
