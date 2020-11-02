429cc29c75f8fdd358b89be4569697564c166e38
Hendryboyz <Hendry.js1247@gmail.com>
2020-11-03 07:58:17 +0800

    fix init error and implement insert function

--- linkedlist.py
+++ linkedlist.py
@@ -5,7 +5,7 @@ class node:
 
 class linkedlist:
     def __init__(self, value):
-        self.head = node(10)
+        self.head = node(value)
         self.tail = self.head
         self.__length = 1
     
@@ -24,12 +24,33 @@ class linkedlist:
         new_node.next = self.head
         self.head = new_node
         self.__length += 1
+
+    def insert(self, index, value):
+        if index == 0:
+            self.prepend(value)
+        elif index == self.__length - 1:
+            self.append(value)
+        else:
+            new_node = node(value)
+            prev = self.__find_handle_node(index)
+            new_node.next = prev.next
+            prev.next = new_node
+            self.__length += 1
+
+    def __find_handle_node(self, index):
+        prev = self.head
+        for i in range(index - 1):
+            if prev.next != None:
+                prev = prev.next
+        return prev
     
-    def traverse(self):
-        element = []
+    # def remove(self, index):
+        # todo
+
+    def print_list(self):
+        elements = []
         cur = self.head
         while (cur != None):
-            element.append(cur.value)
+            elements.append(cur.value)
             cur = cur.next
-        return element
-
+        print(elements)
