9802e2917c91fc118a264cdafca0b382bb97170e
Hendryboyz <Hendry.js1247@gmail.com>
2020-11-19 06:07:49 +0800

    Check avltree balance after insertion node

--- avltree.py
+++ avltree.py
@@ -15,30 +15,41 @@ class AVLTree:
     if self.root == None:
       self.root = new_node
     else:
-      parent_node = self.__loop_find_parent(value)
+      path = self.__find_insertion_path(value)
+      parent_node = path[-1]
       if value >= parent_node.value:
         parent_node.right = new_node
       else:
         parent_node.left = new_node
+      
+      if self.__is_balance(path[2]) == False:
+        self.__rotation(path[1], path[2])
+      if path[1] != None and self.__is_balance(path[1]) == False:
+        self.__rotation(path[0], path[1])
     
-    print ('Current tree height is ' + str(self.__get_tree_height(self.root)))
-    
-  def __loop_find_parent(self, value):
-    parent = self.root
+  def __find_insertion_path(self, value):
+    path = [None] * 3
+    current = self.root
     while True:
-      if value >= parent.value:
-        if parent.right == None:
+      path[0] = path[1]
+      path[1] = path[2]
+      path[2] = current
+      if value >= current.value:
+        if current.right == None:
           break
         else:
-          parent = parent.right
+          current = current.right
       else:
-        if parent.left == None:
+        if current.left == None:
           break
         else:
-          parent = parent.left
+          current = current.left
 
-    return parent
+    return path
   
+  def __is_balance(self, node: BinaryTreeNode):
+    return abs(self.__get_tree_height(node.left) - self.__get_tree_height(node.right)) <= 1
+
   def __get_tree_height(self, root: BinaryTreeNode) -> int:
     if root == None:
       return -1
@@ -47,6 +58,9 @@ class AVLTree:
     else:
       return max(self.__get_tree_height(root.left), self.__get_tree_height(root.right)) + 1
   
+  def __rotation(self, parent: BinaryTreeNode, target: BinaryTreeNode):
+    pass
+
   def __left_rotation(self, parent: BinaryTreeNode, current: BinaryTreeNode):
     right_node = current.right
     current.right = right_node.left
@@ -64,3 +78,9 @@ class AVLTree:
       parent.left = new_child
     else:
       parent.right = new_child
+
+tree = AVLTree()
+tree.insert(1)
+tree.insert(2)
+tree.insert(3)
+tree.insert(4)
